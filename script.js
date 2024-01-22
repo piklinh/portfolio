63c3e95e2c586f951248826049e2e30e85bd2cd2
piklinh <hplesson@gmail.com>
2024-01-22 17:24:59 -0500

    major updates, subpages :sparkles:

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const portfolio = {};
 
 portfolio.typeEffect = () => {
     let typed = new Typed(".type",  {
-        strings: ["", "Web Developer", "Designer", "Copywriter", "Bookworm", "Calligrapher"],
+        strings: ["", "Web Developer", "Designer", "Copywriter", "CRM Email Specialist", "Bookworm"],
         typeSpeed: 75,
         backSpeed: 75,
         loop: true
@@ -14,18 +14,41 @@ portfolio.parallax = () => {
     const parallaxInstance = new Parallax(butterfly);
 }
 
-portfolio.hamburgerMenu = () => {
-    $('.hamburger').click(function(){
-        $('.navContainer').toggleClass('active');
-    })
+portfolio.hamburger = () => {
+    const hamburger = document.querySelector('.burger');
+    const navMenu = document.querySelector('.navContainer');
+    
+    hamburger.addEventListener('click', mobileMenu);
+        function mobileMenu() {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    }
+
+    const navLink = document.querySelectorAll(".navLink");
+
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+        function closeMenu() {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+}
+
+portfolio.accordion = () =>{
+    const acc = document.getElementsByClassName('accordionBox');
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener('click', function() {
+            this.classList.toggle('active')
+        })
+    }
 }
 
 // Initiate the app
 portfolio.init = () => {
     AOS.init();
+    portfolio.hamburger();
+    portfolio.accordion();
     portfolio.typeEffect();
     portfolio.parallax();
-    portfolio.hamburgerMenu();
 }
 
 // Document ready - load HTML/CSS, then initialize JQ!
